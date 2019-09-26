a13780b9f4656fbabdf2f5117f5b9a8ad72ccdb9
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-09-26 14:30:23 +0900

    commentspage test

--- tests/Feature/CommentsPageTest.php
+++ tests/Feature/CommentsPageTest.php
@@ -15,8 +15,19 @@ class CommentsPageTest extends TestCase
    */
   public function testExample()
   {
-    $response = $this->get('/');
+    $response = $this->get('/comments');
+    $response->assertStatus(200);
+  }
 
+  public function testExample2()
+  {
+    $response = $this->get('/comments/3');
+    $response->assertStatus(200);
+  }
+
+  public function testExample3()
+  {
+    $response = $this->get('/comments/3/edit');
     $response->assertStatus(200);
   }
 }
