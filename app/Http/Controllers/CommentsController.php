c67bad0a22dacf13739e582b0af1b50507cc7914
Koichiro Rinsaka <rinsaka@ba.kobegakuin.ac.jp>
2019-09-26 11:13:25 +0900

    welcome

--- app/Http/Controllers/CommentsController.php
+++ app/Http/Controllers/CommentsController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Comment;   // 追加する
 
 class CommentsController extends Controller
 {
   public function index()
   {
-    dd("コメントのindex");
+    $comments = Comment::get();    // SELECT * FROM comments; のイメージ
+    dd($comments);
   }
 }
